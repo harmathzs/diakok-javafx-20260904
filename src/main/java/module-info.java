531ca0ce55854dd808eb71050be09b9d7a5637f8
Zsolt Harmath <harmathzs@gmail.com>
2026-09-04 11:37:23 +0200

    Create JavaFX project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.diakok20260904 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.diakok20260904 to javafx.fxml;
+    exports com.example.diakok20260904;
+}
