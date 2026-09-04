694c95fd0486c80ccc7f404e1166666094a6d85f
Zsolt Harmath <harmathzs@gmail.com>
2026-09-04 12:32:49 +0200

    Adjust window and icon

--- src/main/java/com/example/diakok20260904/DiakokApplication.java
+++ src/main/java/com/example/diakok20260904/DiakokApplication.java
@@ -3,6 +3,7 @@ package com.example.diakok20260904;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +12,11 @@ public class DiakokApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(DiakokApplication.class.getResource("diakok-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 608, 366);
         stage.setTitle("Diákok");
         stage.setScene(scene);
+        Image windowIconImage = new Image("file:icons/diak.png");
+        stage.getIcons().add(windowIconImage);
         stage.show();
     }
 
