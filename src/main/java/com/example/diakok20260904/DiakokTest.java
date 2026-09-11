df518c83cf51e188b5a379acb8b91367fec2864a
Zsolt Harmath <harmathzs@gmail.com>
2026-09-11 09:31:48 +0200

    Import Junit

--- src/main/java/com/example/diakok20260904/DiakokTest.java
+++ src/main/java/com/example/diakok20260904/DiakokTest.java
@@ -1,4 +1,10 @@
 package com.example.diakok20260904;
 
+import org.junit.Test;
+
 public class DiakokTest {
+    @Test
+    public void testApp() {
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.diakok20260904 {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires junit;
 
     opens com.example.diakok20260904 to javafx.fxml;
     exports com.example.diakok20260904;
