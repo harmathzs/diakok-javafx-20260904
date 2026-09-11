fb0bcd3e30af240495f2368f686336c9df35748d
Zsolt Harmath <harmathzs@gmail.com>
2026-09-11 12:02:36 +0200

    Test static methods

--- src/main/java/com/example/diakok20260904/DiakokAppTest.java
+++ src/main/java/com/example/diakok20260904/DiakokAppTest.java
@@ -1,26 +1,42 @@
 package com.example.diakok20260904;
 
-import org.junit.Test;
+import org.junit.*;
 
 public class DiakokAppTest {
     public static Boolean isRunningTest = false;
 
-    @Test
-    public void testMain() {
+    @BeforeClass
+    public static void testBeforeOnce() {
+        System.out.println("BeforeClass");
+    }
+    @AfterClass
+    public static void testAfterOnce() {
+        System.out.println("AfterClass");
+    }
+
+    @Before
+    public void testBefore() {
+        System.out.println("Before");
         isRunningTest = true;
-        DiakokApplication.main(null);
+    }
+    @After
+    public void testAfter() {
+        System.out.println("After");
         isRunningTest = false;
     }
 
     @Test
+    public void testMain() {
+        DiakokApplication.main(null);
+    }
+
+    @Test
     public void testStart() {
-        isRunningTest = true;
         DiakokApplication app = new DiakokApplication();
         try {
             app.start(null);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        isRunningTest = false;
     }
 }
